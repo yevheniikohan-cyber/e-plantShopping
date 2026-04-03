5759a7fd382e9803233e85313c95628a165d300b
yevheniikohan-cyber <yevheniikohan@gmail.com>
2026-04-03 13:47:59 -0400

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is an online plant shop that offers a wide variety of
+        indoor and outdoor plants. Our mission is to bring nature closer to your home
+        by providing high-quality plants at affordable prices.
+      </p>
+      <p>
+        We are passionate about greenery and believe plants improve well-being,
+        air quality, and overall happiness.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
